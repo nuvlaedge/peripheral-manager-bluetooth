3a2290c5ecb573a214587453c4568adcf6fca8ae
Francisco <fran.abm94@gmail.com>
2020-10-09 11:45:02 +0200

    testing push to nuvla

--- code/manager.py
+++ code/manager.py
@@ -5,14 +5,9 @@
 """NuvlaBox Peripheral Manager Bluetooth
 This service provides bluetooth device discovery.
 """
-<<<<<<< HEAD
-import bluetooth 
-from gattlib import DiscoveryService # Used for BLE discovery
-=======
 
 import bluetooth
 from gattlib import DiscoveryService  # Used for BLE discovery
->>>>>>> 6c2823b0baac7a533f2e97d1b2925be18b8f8d19
 import logging
 import requests
 import sys
@@ -21,9 +16,8 @@ from threading import Event
 import json
 import docker
 from packaging import version
-import nuvla
-
-image = 'bluetooth_information:{}'
+from nuvla.api import Api
+import os
 
 
 def init_logger():
@@ -39,45 +33,21 @@ def init_logger():
     root.addHandler(handler)
 
 
-def wait_bootstrap(healthcheck_endpoint="http://agent/api/healthcheck"):
+def wait_bootstrap(context_file):
     """
-    Simply waits for the NuvlaBox to finish bootstrapping, by pinging
-        the Agent API
+    Waits for the NuvlaBox to finish bootstrapping, by checking
+        the context file.
     :returns
     """
+    is_context_file = False
 
-    logging.info("Checking if NuvlaBox has been initialized...")
-
-    r = requests.get(healthcheck_endpoint)
-
-    while not r.ok:
+    while not is_context_file:
         time.sleep(5)
-        r = requests.get(healthcheck_endpoint)
+        if os.path.isfile(context_file):
+            is_context_file = True
 
     logging.info('NuvlaBox has been initialized.')
     return
-
-
-def publish(url, assets):
-    """
-    API publishing function.
-    """
-
-    x = requests.post(url, json=assets)
-    return x.json()
-
-
-def send(url, assets):
-    """ Sends POST request for registering new peripheral """
-
-    logging.info("Sending Bluetooth Device information to Nuvla")
-    return publish(url, assets)
-
-
-def remove(url, assets):
-    logging.info("Removing Bluetooth Device from Nuvla")
-    x = requests.delete(url, json=assets)
-    return x.json()
 
 
 def bluetoothCheck(api_url, currentDevices):
@@ -105,43 +75,6 @@ def bluetoothCheck(api_url, currentDevices):
     return False
     
 
-def getCurrentImageVersion(client):
-
-    peripheralVersion = ''
-    bluetoothVersion = ''
-    
-    for container in client.containers.list():
-        img, tag = container.image.attrs['RepoTags'][0].split(':')
-        if img == 'nuvlabox/peripheral-manager-bluetooth':
-            peripheralVersion = tag
-        elif img == image:
-            bluetoothVersion = tag
-
-    if version.parse(peripheralVersion) > version.parse(bluetoothVersion):
-        return peripheralVersion
-
-    else:
-        return '0.0.1'
-
-
-def launchSideContainer():
-    client = docker.from_env()
-    volumes = {}
-
-    currentVersion = getCurrentImageVersion(client)
-    img = image.format(currentVersion)
-
-    dbus = '/var/run/dbus'
-    volumes[dbus] = {'bind': dbus, 'mode': 'ro'}
-
-    if len(client.images.list(img)) == 0 and currentVersion != '':
-        logging.info('Build CUDA Cores Image')
-        client.images.build(path='.', tag=img, dockerfile='Dockerfile.bluetooth')
-
-    output = client.containers.run(img, network_mode="host", volumes=volumes)
-    print('OUTPUT: {}'.format(output))
-    return output
-
 def deviceDiscovery():
     """
     Return all discoverable bluetooth devices.
@@ -168,7 +101,7 @@ def compareBluetooth(bluetooth, ble):
     return output
 
 
-def bluetoothManager():
+def bluetoothManager(nuvlabox_id, nuvlabox_version):
 
     output = {}
 
@@ -186,6 +119,8 @@ def bluetoothManager():
     if len(bluetooth) > 0:
         for device in bluetooth:
             output[device[0][0]] = {
+                    "parent": nuvlabox_id,
+                    "version": nuvlabox_version,
                     "available": True,
                     "name": device[0][1],
                     "classes": [],
@@ -196,13 +131,61 @@ def bluetoothManager():
     return output
 
 
+def add(data, api_url, activated_path, cookies_file):
+
+    api = Api(api_url)
+
+    activated = json.load(open(activated_path))
+    api_key = activated['api-key']
+    secret_key = activated['secret-key']
+    
+    login = api.login_apikey(api_key, secret_key)
+
+    if login.status_code == 201 or login.status_code == 200:
+        response = api.add('nuvlabox-peripheral', data).data
+        if response['status'] == 200:
+            return response['resource-id']
+        else:
+            return ''
+    else:
+        return ''
+
+
+def remove(resource_id, api_url, activated_path, cookies_file):
+    
+    api = Api(api_url)
+
+    activated = json.load(open(activated_path))
+    api_key = activated['api-key']
+    secret_key = activated['secret-key']
+    
+    login = api.login_apikey(api_key, secret_key)
+
+    if login.status_code == 201 or login.status_code == 200:
+        response = api.delete(resource_id).data
+        if response['status'] == 200:
+            return response['resource-id']
+        else:
+            return ''
+    else:
+        return ''
+
 if __name__ == "__main__":
+
+    activated_path = '/home/quietswami/nuvlabox/shared/.activated'
+    context_path = '/home/quietswami/nuvlabox/shared/.context'
+    cookies_file = '/home/quietswami/nuvlabox/shared/cookies'
+
+    context = json.load(open(context_path))
+
+    version = context['version']
+    nuvlabox_id = context['id']
 
     print('BLUETOOTH MANAGER STARTED')
 
     init_logger()
 
-    API_BASE_URL = "http://agent/api"
+    API_BASE_URL = "https://nuvla.io"
 
     # wait_bootstrap()
 
@@ -211,66 +194,38 @@ if __name__ == "__main__":
     # e = Event()
 
     devices = {}
-
+    
     while True:
-<<<<<<< HEAD
-        
-        print('Starting Search')
-        current_network = launchSideContainer()
-        print('Search Done')
-        if current_network != network and current_network != []:
-=======
 
-        current_devices = bluetoothManager()
+        current_devices = bluetoothManager(nuvlabox_id, version)
         print('CURRENT DEVICES: {}\n'.format(current_devices), flush=True)
         
-        if current_devices != devices and current_devices:
+    #     if current_devices != devices and current_devices:
 
-            devices_set = set(devices.keys())
-            current_devices_set = set(current_devices.keys())
->>>>>>> 6c2823b0baac7a533f2e97d1b2925be18b8f8d19
+    #         devices_set = set(devices.keys())
+    #         current_devices_set = set(current_devices.keys())
 
-            publishing = current_devices_set - devices_set
-            removing = devices_set - current_devices_set
+    #         publishing = current_devices_set - devices_set
+    #         removing = devices_set - current_devices_set
 
-            for device in publishing:
+    #         for device in publishing:
 
-<<<<<<< HEAD
-                print('PUBLISHING: {}'.format(current_network[device]))
-                # peripheral_already_registered = bluetoothCheck(API_URL, current_network[device])
+    #             peripheral_already_registered = \
+    #                 bluetoothCheck(API_URL, current_devices[device])
 
-                # if peripheral_already_registered:
-                    # send(API_URL, current_network[device])
-                network[device] = current_network[device]
+    #             if not peripheral_already_registered:
+    #                 print('PUBLISHING: {}'.format(current_devices[device]), flush=True)
+    #                 resource_id = add(testPut, 'https://nuvla.io', activated_path, cookies_file)
+    #                 devices[device] = {'resource_id': resource_id, 'message': current_devices[device]}
 
-            for device in removing:
+    #         for device in removing:
 
-                print('REMOVING: {}'.format(network[device]))
-                # peripheral_already_registered = bluetoothCheck(API_URL, network[device])
-                
-                # if not peripheral_already_registered:
-                    # remove(API_URL, network[device])
-                del network[device]
+    #             peripheral_already_registered = \
+    #                 bluetoothCheck(API_URL, devices[device])
 
-        # e.wait(timeout=90)
-=======
-                peripheral_already_registered = \
-                    bluetoothCheck(API_URL, current_devices[device])
+    #             if peripheral_already_registered:
+    #                 print('REMOVING: {}'.format(devices[device]), flush=True)
+    #                 remove(API_URL, devices[device]['resource_id'])
+    #                 del devices[device]
 
-                if not peripheral_already_registered:
-                    print('PUBLISHING: {}'.format(current_devices[device]), flush=True)
-                    send(API_URL, current_devices[device])
-                    devices[device] = current_devices[device]
-
-            for device in removing:
-
-                peripheral_already_registered = \
-                    bluetoothCheck(API_URL, devices[device])
-
-                if peripheral_already_registered:
-                    print('REMOVING: {}'.format(devices[device]), flush=True)
-                    remove(API_URL, devices[device])
-                    del devices[device]
-
-        e.wait(timeout=90)
->>>>>>> 6c2823b0baac7a533f2e97d1b2925be18b8f8d19
+    #     e.wait(timeout=90)
--- code/nuvla.py
+++ code/nuvla.py
@@ -2,6 +2,7 @@ import requests
 import os
 import sys
 import json
+import http.cookiejar as cookiejar
 
 def login_push(session_template, url, cookie_file):
     return requests.post(url, headers={'content-type':'application/json', 'accept':'application/json'}, cookies=cookie_file, data=session_template)
@@ -11,7 +12,11 @@ def login(activated_path, nuvla_endpoint, cookies_file, extra_flags):
     activated = json.load(open(activated_path))
     api_key = activated['api-key']
     secret_key = activated['secret-key']
-
+    if cookies_file:
+        cookie = cookiejar.LWPCookieJar(filename=cookies_file)
+        print(cookie)
+    else:
+        cookie = ''
     # Then we have to login first
     session_template = {
         "template": {
@@ -24,12 +29,12 @@ def login(activated_path, nuvla_endpoint, cookies_file, extra_flags):
     url = '{}/api/session'.format(nuvla_endpoint)
 
     if cookies_file:
-        session_alive = requests.get(url, headers={'content-type':'application/json'}, cookies=cookies_file)
-
+        session_alive = requests.get(url, headers={'content-type':'application/json'}, cookies=cookie)
+        print(session_alive)
         if session_alive:
-            login_push(session_template, url, cookies_file)
+            print(login_push(session_template, url, cookie))
     else:
-        login_push(session_template, url, cookies_file)
+        print(login_push(session_template, url, cookie))
 
 def add_bluetooth_peripheral(put_json, nuvla_endpoint, nuvla_id, activated_path, cookies_file, extra_flags):
    
@@ -44,3 +49,12 @@ def remove_bluetooth_peripheral(resource_id, nuvla_endpoint, data, activated_pat
 
     url = '{}/api/{}'.format(nuvla_endpoint, resource_id) 
     r = requests.delete(url, headers={'content-type':'application/json'}, cookies=cookies_file)
+
+
+
+
+if __name__ == "__main__":
+    activated_path = '/home/quietswami/nuvlabox/shared/.activated'
+    cookies_file = '/home/quietswami/nuvlabox/shared/cookies'
+
+    login(activated_path, 'http://nuvla.io/', cookies_file, {})
