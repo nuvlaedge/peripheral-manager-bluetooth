94503e8c720b28e0d02ecf5df80a4555e1de5c26
Francisco <fran.abm94@gmail.com>
2020-10-15 11:16:45 +0200

    updated bootstrap

--- code/manager.py
+++ code/manager.py
@@ -46,10 +46,12 @@ def wait_bootstrap(context_file, base_peripheral_path, peripheral_path):
             logging.info('Context file found')
         logging.info('Waiting for context file')
 
-    while not os.path.isdir(base_peripheral_path):
-        logging.info('Wating for peripheral dir...')
-        if not os.path.isdir(peripheral_path):
+    peripheral = False
+    while not peripheral:
+        logging.info('Wating for peripheral directory...')
+        if os.path.isdir(base_peripheral_path) and not os.path.isdir(peripheral_path):
             os.mkdir(peripheral_path)
+            peripheral = True
 
     logging.info('NuvlaBox has been initialized.')
     return
