d89ead2c7d9db2ac79ba92270ee73f97386082bb
Francisco <fran.abm94@gmail.com>
2020-10-15 08:43:10 +0200

    merging

--- code/manager.py
+++ code/manager.py
@@ -5,14 +5,9 @@
 """NuvlaBox Peripheral Manager Bluetooth
 This service provides bluetooth device discovery.
 """
-<<<<<<< HEAD
-import bluetooth 
-from gattlib import DiscoveryService # Used for BLE discovery
-=======
 
 import bluetooth
 from gattlib import DiscoveryService  # Used for BLE discovery
->>>>>>> 6c2823b0baac7a533f2e97d1b2925be18b8f8d19
 import logging
 import requests
 import sys
@@ -213,29 +208,17 @@ if __name__ == "__main__":
     devices = {}
 
     while True:
-<<<<<<< HEAD
         
         print('Starting Search')
         current_network = launchSideContainer()
         print('Search Done')
         if current_network != network and current_network != []:
-=======
-
-        current_devices = bluetoothManager()
-        print('CURRENT DEVICES: {}\n'.format(current_devices), flush=True)
-        
-        if current_devices != devices and current_devices:
-
-            devices_set = set(devices.keys())
-            current_devices_set = set(current_devices.keys())
->>>>>>> 6c2823b0baac7a533f2e97d1b2925be18b8f8d19
 
             publishing = current_devices_set - devices_set
             removing = devices_set - current_devices_set
 
             for device in publishing:
 
-<<<<<<< HEAD
                 print('PUBLISHING: {}'.format(current_network[device]))
                 # peripheral_already_registered = bluetoothCheck(API_URL, current_network[device])
 
@@ -253,24 +236,3 @@ if __name__ == "__main__":
                 del network[device]
 
         # e.wait(timeout=90)
-=======
-                peripheral_already_registered = \
-                    bluetoothCheck(API_URL, current_devices[device])
-
-                if not peripheral_already_registered:
-                    print('PUBLISHING: {}'.format(current_devices[device]), flush=True)
-                    send(API_URL, current_devices[device])
-                    devices[device] = current_devices[device]
-
-            for device in removing:
-
-                peripheral_already_registered = \
-                    bluetoothCheck(API_URL, devices[device])
-
-                if peripheral_already_registered:
-                    print('REMOVING: {}'.format(devices[device]), flush=True)
-                    remove(API_URL, devices[device])
-                    del devices[device]
-
-        e.wait(timeout=90)
->>>>>>> 6c2823b0baac7a533f2e97d1b2925be18b8f8d19
